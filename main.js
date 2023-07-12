540def6c02b96f5e2b434c20a233bdc6be1567e0
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-12 05:38:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+
+let flag=0;
+
+function controller(x){
+  flag =flag+x;
+  showslider(flag);
+}
+
+showslider(flag); 
+
+function showslider(num) {
+  let slide = document.getElementsByClassName('slider');
+
+  if(num==slide.length){
+    flag=0;
+    num=0;
+  }
+  if(num<0) {
+    flag=slide.length-1;
+    num=slide.length-1;
+  }
+
+  for(let y of slide){
+    y.style.display ="none";
+  }
+  // console.log(num);
+  slide[num].style.display ="block";
+}
+
+
